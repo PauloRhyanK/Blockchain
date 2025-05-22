487056e23139a2f008e21ca4da8b08f6f91bf492
Paulo Rhyan Kuster <103347114+PauloRhyanK@users.noreply.github.com>
2025-05-22 16:18:50 +0000

    feat: validação do blockchain

--- thread_miner.py
+++ thread_miner.py
@@ -57,21 +57,52 @@ class Blockchain:
             return last_block.index;
         return 0
     
-    def check_block(self, hash):
+    def check_duplicate_hash(self, hash):
         for index_chain in range(len(self.chain)):
             if self.chain[index_chain].hash == hash:
                 return False
         return True
+    
+    def check_block(self, new_block):
+        if not self.check_duplicate_hash:
+            return False
+        if self.get_latest_block().hash != new_block.previous_hash:
+            return False
+        return True
+    
+    def check_blockchain(self):
+        validChain = True
+        previous_block = self.chain[0]
+        
+        invalid_blocks = []
+        for indexBlock in range(1,len(self.chain)):
+            current_block = self.chain[indexBlock]
+            if previous_block.hash != current_block.previous_hash:
+                invalid_blocks.append(current_block.index)
+                validChain = False
+            previous_block = current_block
+            
+        if not validChain:
+            print(F" \n Blockchain invalido! \n Os seguintes blocos apresentam inconsistencias.")
+            for index in range(len(invalid_blocks)):
+                print(F"Bloco index: {self.chain[invalid_blocks[index]].index}")
+        else:
+            print("Blockchain Válido!")
+        return validChain
+            
+            
             
     
     def add_block(self, new_block, stop_event):
         new_block.previous_hash = self.get_latest_block().hash
         sucessMiner = new_block.mine_block(self.difficulty, stop_event)
-        if(sucessMiner and self.check_block(new_block.hash)):
+        if(sucessMiner and self.check_block(new_block)):
             self.chain.append(new_block)
+    
+    
         
             
-def concurrent_mining(num_threads, difficulty, newBlock = False ,blockchain = False):
+def concurrent_mining(num_threads, difficulty = 2, newBlock = False ,blockchain = False):
     
     block_existing = False
     if(newBlock and blockchain):
@@ -103,21 +134,32 @@ def concurrent_mining(num_threads, difficulty, newBlock = False ,blockchain = Fa
     elapsed_time = time.time() - start_time
     print(f"\n Mineracao com {num_threads} threads concluidas em {elapsed_time:.2f} segundos.")
     
+
+
 if __name__ == "__main__":
     # concurrent_mining(num_threads=4,difficulty=2)
     testeBlock = Blockchain(4)
     testeBlock.create_genesis_block()   
     queueDataList = [
-        "Dado 1",
-        "Dado 2",
-        "Dado 3"
+        "Alonso passo 20 Blockcoin para ZecaUrubu",
+        "Patolino passou 12919 BlockCoin para Pernalonga",
+        "Finn passou 69 BlockCoin para FirePrincess",
+        "Mickey passou 150 Blockcoin para Minnie",
+        "Goku passo 5000 BlockCoin para Vegeta",
+        "Batman passou 75 BlockCoin para Robin",
+        "Mario passo 100 Blockcoin para Luigi",
+        "Tony Stark passo 10000 Blockcoin para Peter Parker",
+        "BobEsponja passo 5 Blockcoin para PatrickEstrela",
+        "Burro passou 88 BlockCoin para Shrek",
     ]
     # print(json.dumps(testeBlock.get_latest_block().__dict__))
+    
     for i in range(0,len(queueDataList)):
         newBlock = Block(testeBlock.get_latest_index() + 1, testeBlock.get_latest_block().previous_hash, time.time(), queueDataList[i] )
-        concurrent_mining(num_threads=10, difficulty=2, newBlock=newBlock, blockchain=testeBlock )
+        concurrent_mining(num_threads=10, newBlock=newBlock, blockchain=testeBlock )
 
     print(json.dumps([block.__dict__ for block in testeBlock.chain], indent=4))
+    testeBlock.check_blockchain()
     
     
     
